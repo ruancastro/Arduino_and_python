ece6993079f71498de5fbf37003770769de4dcf7
Ruan  Castro <60793917+ruancastro@users.noreply.github.com>
2020-07-28 17:23:37 -0300

    Add files via upload
    Você primeiro deve rodar o Sketch do arduino e então, ler a comunicação serial através do arquivo python

--- Dht11 (Temperature)/main_sensor.py
+++ Dht11 (Temperature)/main_sensor.py
@@ -1,1 +1,19 @@
+import serial
+import pandas as pd
 
+x = []
+y = []
+ser = serial.Serial('/dev/ttyUSB0', 9600)
+for i in range(11):
+    arduinoData = ser.readline().decode('utf-8"')
+    x.append(arduinoData[0:5]) 
+    y.append(arduinoData[6:11])
+    # print(type(arduinoData))
+    # print(arduinoData)
+    print("\nUmidade {}\t Temperatura {}\n".format(x[i],y[i]))
+
+df = pd.DataFrame(columns=['Umidade','Temperatura'])
+df['Umidade'] = x
+df['Temperatura'] = y
+
+print(df.head(10))
